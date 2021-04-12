30f409f76aee563c38797a44253bed3e7848a5fa
cagatayasar <cagatayasar@hotmail.com>
2021-04-13 02:09:08 +0300

    deleted comments

--- Assets/Scripts/UnitDragHandler.cs
+++ Assets/Scripts/UnitDragHandler.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class UnitDragHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler //, IBeginDragHandler, IDragHandler, IEndDragHandler,
+public class UnitDragHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
 {
     public int index;
 
@@ -17,28 +17,11 @@ public class UnitDragHandler : MonoBehaviour, IPointerEnterHandler, IPointerExit
         if (dragging) {
             transform.position = slotInitialPosition + Camera.main.ScreenToWorldPoint(Input.mousePosition) - mouseInitialPosition;
             Player.instance.DragUnitAt(index, transform.position);
-            // show hovered area using transform.position
         }
     }
 
-    // public void OnBeginDrag(PointerEventData eventData) {
-    //     slotInitialPosition = transform.position;
-    //     mouseInitialPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    //     // show placeable areas
-    // }
-
-    // public void OnDrag(PointerEventData eventData) {
-    //     transform.position = slotInitialPosition + Camera.main.ScreenToWorldPoint(Input.mousePosition) - mouseInitialPosition;
-    //     // show hovered area using transform.position
-    // }
-
-    // public void OnEndDrag(PointerEventData eventData) {
-    //     transform.position = slotInitialPosition;
-    // }
-
     public void OnPointerEnter(PointerEventData eventData) {
         // Debug.Log("Pointer entered " + unitIndex);
-        
     }
 
     public void OnPointerExit(PointerEventData eventData) {
